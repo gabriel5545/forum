ee2bdab06d2d7c3379ce6703bf967c9195984415
gabriel <gabrielmar5545@gmail.com>
2017-09-05 15:18:07 +0000

    hope this works

---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Forum Threads</div>
+
+                <div class="panel-body">
+                    <div class="list-group">
+                      <a href="#" class="list-group-item">
+                        Dapibus ac facilisis in
+                      </a>
+                      <a href="#" class="list-group-item">
+                        Morbi leo risus
+                      </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
